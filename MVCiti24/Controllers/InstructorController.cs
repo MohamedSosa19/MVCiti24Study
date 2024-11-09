b3d14cb0881248fb9d89d8e54c96be3e1b3a8c61
mohamedsosa626@gmail.com <mohamedsosa626@gmail.com>
2024-11-09 13:58:29 +0200

    Add&Edit Instructor

--- MVCiti24/Controllers/InstructorController.cs
+++ MVCiti24/Controllers/InstructorController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVCiti24.Models;
+using MVCiti24.ViewModels;
 
 namespace MVCiti24.Controllers
 {
@@ -20,6 +21,101 @@ namespace MVCiti24.Controllers
             Instructor instructorDetails = contectDB.Instructors.Include(d=>d.Department).Include(c => c.Course).FirstOrDefault(e => e.Id == id);
                
             return View("Details", instructorDetails);
+        }
+
+        public IActionResult AddInstructor()
+        {
+            List<Department> DepartmentList = contectDB.Departments.ToList();
+            List<Course> CoursesList = contectDB.Courses.ToList();
+            Instructor instructor = new Instructor();
+            InstructorWithDeptCourseList instructorVM= new InstructorWithDeptCourseList();
+            //Mapping to make VM As instructor Model
+            instructorVM.Id = instructor.Id;
+            instructorVM.Name=instructor.Name;
+            instructorVM.Image=instructor.Image;
+            instructorVM.Address=instructor.Address;
+            instructorVM.Salary=instructor.Salary;
+            instructorVM.DepartmentId = instructor.DepartmentId;
+            instructorVM.CourseId=instructor.CourseId;
+            instructorVM.DeptList=DepartmentList;
+            instructorVM.CourseList = CoursesList;
+            return View("AddInstructor", instructorVM);
+        }
+
+  
+        [HttpPost]
+        public IActionResult SaveInstructor(InstructorWithDeptCourseList instructorVM)
+        {
+            if (ModelState.IsValid)
+            {
+                Instructor instructor = new Instructor
+                {
+                    Name = instructorVM.Name,
+                    Image = instructorVM.Image,
+                    Salary = instructorVM.Salary,
+                    Address = instructorVM.Address,
+                    DepartmentId = instructorVM.DepartmentId,
+                    CourseId = instructorVM.CourseId
+                };
+
+                contectDB.Instructors.Add(instructor);
+                contectDB.SaveChanges();
+                return RedirectToAction("AllInstructor");
+            }
+
+ 
+            foreach (var modelState in ModelState.Values)
+            {
+                foreach (var error in modelState.Errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+            instructorVM.DeptList = contectDB.Departments.ToList();
+            instructorVM.CourseList = contectDB.Courses.ToList();
+
+            return View("AddInstructor", instructorVM);
+        }
+
+
+        public IActionResult EditInstructor(int Id)
+        {
+            List<Department> DepartmentList = contectDB.Departments.ToList();
+            List<Course> CoursesList = contectDB.Courses.ToList();
+            Instructor instructor = contectDB.Instructors.FirstOrDefault(e => e.Id == Id);
+            InstructorWithDeptCourseList instructorVM = new InstructorWithDeptCourseList();
+
+            instructorVM.Id = instructor.Id;
+            instructorVM.Name = instructor.Name;
+            instructorVM.Image = instructor.Image;
+            instructorVM.Address = instructor.Address;
+            instructorVM.Salary = instructor.Salary;
+            instructorVM.DepartmentId = instructor.DepartmentId;
+            instructorVM.CourseId = instructor.CourseId;
+            instructorVM.DeptList = DepartmentList;
+            instructorVM.CourseList = CoursesList;
+            return View("EditInstructor", instructorVM);
+        }
+
+        [HttpPost]
+        public IActionResult EditInstructor(int Id, InstructorWithDeptCourseList instructorVM)
+        {
+            if(ModelState.IsValid)
+            {
+                Instructor instructor = contectDB.Instructors.FirstOrDefault(e => e.Id == Id);
+                instructor.Id = instructorVM.Id;
+                instructor.Name = instructorVM.Name;
+                instructor.Image= instructorVM.Image;
+                instructor.Address= instructorVM.Address;
+                instructor.Salary= instructorVM.Salary;
+                instructor.DepartmentId= instructorVM.DepartmentId;
+                instructor.CourseId= instructorVM.CourseId;
+                contectDB.SaveChanges();
+                return RedirectToAction("AllInstructor");
+            }
+            instructorVM.DeptList = contectDB.Departments.ToList();
+            instructorVM.CourseList = contectDB.Courses.ToList();
+            return View("EditInstructor", instructorVM);
         }
     }
 }
---
+++ MVCiti24/ViewModels/InstructorWithDeptCourseList.cs
@@ -0,0 +1,24 @@
+﻿using MVCiti24.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCiti24.ViewModels
+{
+    public class InstructorWithDeptCourseList
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        [Range(2000,15000)]
+        public int Salary { get; set; }
+        public string? Address { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+        [ForeignKey("Course")]
+        public int? CourseId { get; set; }
+
+        public List<Department> DeptList { get; set; } = new List<Department>();
+        public List<Course> CourseList { get; set; } = new List<Course>();
+    }
+}
