b9d04c26e180b708da952307e5210db36516a618
mohamedsosa626@gmail.com <mohamedsosa626@gmail.com>
2024-11-20 00:48:35 +0200

    Day6

---
+++ MVCiti24/Controllers/ResultsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCiti24.Models;
+
+namespace MVCiti24.Controllers
+{
+    public class ResultsController : Controller
+    {
+        ITIContectDB contectDB = new ITIContectDB();
+        public IActionResult Result(int traineeId,int courseId)
+        {
+            var results=contectDB.CrsResults.Include(cr=>cr.Trainees).
+                        Include(cr=>cr.Courses).
+                        FirstOrDefault(cr=>cr.TraineeId==traineeId && cr.CourseId==courseId);
+
+            if (results is null)
+                return NotFound("results Not Found");
+
+            var course=contectDB.Courses.FirstOrDefault(c=>c.Id==courseId);
+            var trainee = contectDB.Trainees.FirstOrDefault(t=>t.Id==traineeId);
+
+            var ResultsVM = new TraineeResults
+            {
+                TraineeName = trainee?.Name,
+                CourseName=course?.Name,
+                Degree=results.Degree,
+                MinDegree=course?.MinDegree??0,
+                IsSuccessful= results.Degree >= (course?.MinDegree ?? 0)
+            };
+
+            return View("Result", ResultsVM);
+        }
+    }
+}
---
+++ MVCiti24/Models/TraineeResults.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCiti24.Models
+{
+    public class TraineeResults
+    {
+        public string? TraineeName { get; set; }
+        public string? CourseName { get; set; }
+        public int Degree { get; set; }
+        public int MinDegree { get; set; }
+        public bool IsSuccessful { get; set; }
+    }
+}
